7e26f89f8fce4252ee20c257b662eef8813e4f5a
theoreticalbts <theoreticalbts@users.noreply.github.com>
2020-11-17 17:36:15 +0000

    Remove debug logging

--- internal/bstore/reqhandler.go
+++ internal/bstore/reqhandler.go
@@ -109,14 +109,11 @@ func GetPreviousHeightIndex( goal types.BlockHeightType, current types.BlockHeig
    var last_h uint64 = 0
    for i := 0; i<=zeros; i++ {
       h := x - (uint64(1) << i)
-      fmt.Printf("      Check index %d, height %d\n", i, h)
       if h < g {
-         fmt.Printf("      Return index %d, height %d\n", i+1, last_h)
          return i+1, types.BlockHeightType(last_h), nil
       }
       last_h = h
    }
-   fmt.Printf("      Return index %d, height %d\n", zeros, last_h)
    return zeros, types.BlockHeightType(last_h), nil
 }
 
@@ -126,10 +123,7 @@ func GetAncestorIdAtHeight( backend BlockStoreBackend, block_id *types.Multihash
    var expected_height types.BlockHeightType
    var has_expected_height bool = false
 
-   fmt.Printf("Begin GetAncestorIdAtHeight(%s, %d)\n", hex.EncodeToString(block_id.Digest), height)
-
    for {
-      fmt.Printf("   Iteration block ID: %s\n", hex.EncodeToString(block_id.Digest))
       vb_key := block_id.Serialize( types.NewVariableBlob() )
 
       record_bytes, err := backend.Get( *vb_key )
@@ -154,8 +148,6 @@ func GetAncestorIdAtHeight( backend BlockStoreBackend, block_id *types.Multihash
          return nil, &DeserializeError{}
       }
       if has_expected_height && (record.BlockHeight != expected_height) {
-         fmt.Println("record height:", record.BlockHeight)
-         fmt.Println("expect height:", expected_height)
          return nil, &UnexpectedHeightError{}
       }
 
@@ -201,7 +193,6 @@ func (handler *RequestHandler) HandleAddBlockReq( req *types.AddBlockReq ) (*typ
 
    if req.BlockToAdd.BlockHeight > 0 {
       previous_heights := GetPreviousHeights(uint64(req.BlockToAdd.BlockHeight))
-      fmt.Println("previous_heights:", previous_heights)
 
       record.PreviousBlockIds = make([]types.Multihash, len(previous_heights))
 
@@ -218,7 +209,6 @@ func (handler *RequestHandler) HandleAddBlockReq( req *types.AddBlockReq ) (*typ
             }
             record.PreviousBlockIds[i] = *previous_id
          }
-         fmt.Printf("PreviousBlockIds[%d]: %s\n", i, hex.EncodeToString(record.PreviousBlockIds[i].Digest))
       }
    } else {
       record.PreviousBlockIds = make([]types.Multihash, 0)

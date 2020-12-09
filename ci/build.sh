00a239ebadebe43890a4dd3a3a328a1f7046394c
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-12-09 12:24:01 -0800

    Move private golang repo config to install.sh #19

--- ci/build.sh
+++ ci/build.sh
@@ -3,10 +3,6 @@
 set -e
 set -x
 
-# These commands can be removed once koinos-types-golang is public
-echo -e "[url \"ssh://git@github.com/\"]\n   insteadOf = https://github.com/\n" >> ~/.gitconfig
-export GOPRIVATE="`go env GOPRIVATE`,github.com/koinos/koinos-types-golang"
-
 go get ./...
 mkdir -p build
 go build -o build/koinos-block-store cmd/koinos-block-store/main.go

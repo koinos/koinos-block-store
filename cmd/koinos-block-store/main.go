d742abca12cdb1974b6d0c19d15796f4e4afeae9
Nathaniel Caldwell <nbcwell@gmail.com>
2021-03-30 15:31:27 -0400

    Add --basedir flag, calculate default value based on OS

--- cmd/koinos-block-store/main.go
+++ cmd/koinos-block-store/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"path"
+	"runtime"
 	"syscall"
 
 	"github.com/dgraph-io/badger"
@@ -18,15 +20,21 @@ const (
 	blockstoreRPC     string = "block_store"
 	blockAccept       string = "koinos.block.accept"
 	blockIrreversible string = "koinos.block.irreversible"
+	appName           string = "block_store"
 )
 
 func main() {
-	var dFlag = flag.StringP("data", "d", "./db", "the database directory")
+	var baseDir = flag.StringP("basedir", "b", getKoinosDir(), "the base directory")
 	var amqpFlag = flag.StringP("amqp", "a", "amqp://guest:guest@localhost:5672/", "AMQP server URL")
 
 	flag.Parse()
 
-	var opts = badger.DefaultOptions(*dFlag)
+	// Costruct the db directory and ensure it exists
+	dbDir := path.Join((*baseDir), appName, "db")
+	ensureDir(dbDir)
+	log.Println(dbDir)
+
+	var opts = badger.DefaultOptions(dbDir)
 	var backend = bstore.NewBadgerBackend(opts)
 	defer backend.Close()
 
@@ -96,4 +104,31 @@ func main() {
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
 	log.Println("Shutting down node...")
+}
+
+func getHomeDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic("There was a problem finding the user's home directory")
+	}
+
+	if runtime.GOOS == "windows" {
+		home = path.Join(home, "AppData")
+	}
+
+	return home
+}
+
+func getKoinosDir() string {
+	return path.Join(getHomeDir(), ".koinos")
+}
+
+func getAppDir(baseDir string, appName string) string {
+	return path.Join(baseDir, appName)
+}
+
+func ensureDir(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, os.ModePerm)
+	}
 }
